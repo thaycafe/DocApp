b272fae3c3ba45ae1e8d9e735e3b61354498a660
thaygneel <thaycic@hotmail.com>
2019-02-01 16:36:59 -0200

    sintaxeLb

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ from kivy.uix.image import Image
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.uix.button import Button
 
 class Inicial(Screen):
     pass
@@ -23,8 +26,20 @@ class SintaxePy(Screen):
 class SintaxeKv(Screen):
     pass
 
+class SintaxeLb(Screen):
+    def clique(self):
+        layout = GridLayout(cols=1)
+        popuplb= Label(text='Você clicou na Label!')
+        popupbt= Button(text='Fechar Popup')
+        layout.add_widget(popuplb)
+        layout.add_widget(popupbt)
+        popup = Popup(title='Função ao clicar na Label', auto_dismiss=False,content=layout, size_hint=(.5, .3))
+        popupbt.bind(on_press=popup.dismiss)
+        popup.open()
+
 class TransicaoTela(ScreenManager):
     transition = NoTransition(duration = 0)
+
 
 class DocApp(App):
     def build(self):
